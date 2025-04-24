7ed7243b9a64df18427f070569fdf6f1f6e43bb0
Franz Ewen Junio Barrera <franzjunio@Franzs-MacBook-Air.local>
2025-04-24 20:52:53 +0200

    add sign in page and login view

--- application/urls.py
+++ application/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("menu", views.menu, name="menu"),
     path("menu/select-pizza/<int:pizza_id>/", views.select_pizza, name="select-pizza"),
+    path("login", views.login, name="login"),
+    # path("signup", views.signup, name="signup"),
 ]
--- application/views.py
+++ application/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Pizza, Drink, Topping
+from django.shortcuts import render, redirect
+from .models import Pizza, Drink, Topping, User
+from django.contrib.auth.hashers import check_password
 
 def home(request):
    all_pizzas = Pizza.objects.all()
@@ -21,3 +22,29 @@ def select_pizza(request, pizza_id):
       "drinks": drinks, 
       "toppings": toppings
    })
+
+def get_user_by_username(username):
+    try:
+        user = User.objects.get(username=username)
+        return user
+    except User.DoesNotExist:
+        return None
+
+def login(request):
+   if request.method == "POST":
+      username = request.POST.get("username")
+      password = request.POST.get("password")
+      print("LOGGED IN USER", username, password)
+
+      user = get_user_by_username(username)
+      if user is None:
+         print("You shall not pass!!!")
+         return render(request, "login.html")
+      
+      if not check_password(password, user.password):
+         print("You shall not pass!!!")
+         return render(request, "login.html")
+      
+      return redirect("/")
+
+   return render(request, "login.html")
