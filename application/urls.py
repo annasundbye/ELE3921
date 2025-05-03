01186e45026418a697fe1746e8285861e526c8ba
Anna Sundbye <annasundbye@Mac.home>
2025-05-03 19:44:26 +0200

    signup user

--- application/urls.py
+++ application/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("menu", views.menu, name="menu"),
     path("menu/select-pizza/<int:pizza_id>/", views.select_pizza, name="select-pizza"),
     path("login", views.login, name="login"),
-    # path("signup", views.signup, name="signup"),
-    path("cart", views.cart, name="cart")
+    path("signup", views.signup, name="signup"),
+    path("cart", views.cart, name="cart"),
+    path("logout", views.logout, name="logout")
 ]
--- application/views.py
+++ application/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Pizza, Drink, Topping, User
-from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 def home(request):
    all_pizzas = Pizza.objects.all()
@@ -16,11 +16,13 @@ def select_pizza(request, pizza_id):
    pizza = Pizza.objects.get(id=pizza_id)
    drinks = Drink.objects.all()
    toppings = Topping.objects.all()
+   available_topping_ids = pizza.available_toppings.values_list('id', flat=True)
    
    return render(request, "select-pizza.html", {
       "pizza": pizza, 
       "drinks": drinks, 
-      "toppings": toppings
+      "toppings": toppings,
+      "available_topping_ids": set(available_topping_ids),
    })
 
 def login(request):
@@ -32,13 +34,34 @@ def login(request):
 
       if user is not None:
          auth_login(request, user) 
-         print("User logged in:", user.username)
          return redirect("/")
       else:
-         print("You shall not pass!!!")
          return render(request, "login.html", {"error": "Invalid credentials."})
 
    return render(request, "login.html")
+
+def signup(request):
+   if request.method == "POST":
+      firstname = request.POST.get("firstname")
+      lastname = request.POST.get("lastname")
+      email = request.POST.get("email")
+      username = request.POST.get("username")
+      password = request.POST.get("password")
+      
+      user = User.objects.create_user(username, password, email)
+      user.last_name = lastname
+      user.first_name = firstname
+      user.save()
+      
+      auth_login(request, user)
+      return redirect("/")
+      
+   return render(request, "signup.html")
+
+def logout(request):
+   auth_logout(request)
+   return redirect("/")
+      
 
 def cart(request):
    if request.method == "POST":
@@ -46,6 +69,11 @@ def cart(request):
          return redirect("/login")
       
       # do more stuff here later
+      quantity = request.POST.get("quantity")
+      size = request.POST.get("size")
+      extra_toppings = request.POST.get("extra-toppings")
+      
+      print(request.POST)
       
       return render(request, "cart.html")
       
