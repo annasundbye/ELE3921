6a9d8751a3cb5cbc21fff503d10e625ba79450b7
Anna Sundbye <annasundbye@Mac.home>
2025-05-03 19:43:31 +0200

    add joke hehe

--- application/management/commands/seed.py
+++ application/management/commands/seed.py
@@ -23,7 +23,7 @@ DEFAULT_PIZZAS = [
    Pizza(name="You can't take this (Pepperoni)", base_price=229.99, icon="🍕", description="Loaded with pepperoni for when you're feeling extra. No cap."),
    Pizza(name="Hotter than hell (Nduja)", base_price=299.99, icon="🌶️", description="Bro. Bro. Bro! This is hot."),
    Pizza(name="Cheesy x 4 (Quatro Formaggi)", base_price=219.99, icon="🧀", description="Just cheese. No explanation needed. It's giving simplicity."),
-   Pizza(name="No nothing (Vegan (none slayed))", base_price=119.99, icon="🥦", description="Veggies on veggies on veggies. It's the responsible choice."),
+   Pizza(name="No nothing (Vegan (none slayed))", base_price=119.99, icon="🥦", description="No animals were harmed making this pizza. It's the responsible choice."),
    Pizza(name="All the good stuff (Hella meat)", base_price=319.99, icon="🥩", description="Hella meat."),
 ]
 
